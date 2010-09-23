464b71b3f5ea18baeac7a36be758dc589f225963
Elias Baixas <elias@eresmas.net>
2010-09-23 13:46:54 +0200

    Removed 'binding' arg to concat for rails 2.3.8
    Rails 2.3.8 warns
    
    DEPRECATION WARNING: The binding argument of #concat is no longer needed.
    Please remove it from your views and helpers.
    (called from globalize_fields_for at
    /lalala...lololo/vendor/plugins/batch_translations/lib/batch_translation.rb:9)
    
    removed the proc.binding to solve the issue

--- lib/batch_translation.rb
+++ lib/batch_translation.rb
@@ -6,8 +6,8 @@ module ActionView
         @index = @index ? @index + 1 : 1
         object_name = "#{@object_name}[translations_attributes][#{@index}]"
         object = @object.translations.find_by_locale locale.to_s
-        @template.concat @template.hidden_field_tag("#{object_name}[id]", object ? object.id : ""), proc.binding
-        @template.concat @template.hidden_field_tag("#{object_name}[locale]", locale), proc.binding
+        @template.concat @template.hidden_field_tag("#{object_name}[id]", object ? object.id : "")
+        @template.concat @template.hidden_field_tag("#{object_name}[locale]", locale)
         @template.fields_for(object_name, object, *args, &proc)
       end
     end
