3bd835037fba90e9cb6275a896e72071552d2f53
Elias Baixas <elias@eresmas.net>
2012-06-10 13:13:25 +0200

    Fixes

--- lib/batch_translation.rb
+++ lib/batch_translation.rb
@@ -5,7 +5,7 @@ module ActionView
         raise ArgumentError, "Missing block" unless block_given?
         @index = @index ? @index + 1 : 1
         object_name = "#{@object_name}[translations_attributes][#{@index}]"
-        object = @object.translations.select{|t| t.locale.to_s == locale}.first || @object.translations.find_by_locale(locale.to_s)
+        object = @object.translations.select{|t| t.locale.to_s == locale}.first || @object.translations.find_by_locale(locale.to_s) || args.first
         @template.concat @template.hidden_field_tag("#{object_name}[id]", object ? object.id : "")
         @template.concat @template.hidden_field_tag("#{object_name}[locale]", locale)
         @template.fields_for(object_name, object, *args, &proc)
